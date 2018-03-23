d85c6a16c8c482bdcaa6aab21b9a50c2d20d8f4c
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-03-23 18:08:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber === "") {
+  if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   }
   var totalCost = total()
